98f6609e71042889a6b2c748dc4e158775052d2a
brunofilipe <bruno.filipe1996@hotmail.com>
2016-10-06 17:18:20 +0100

    first commit

---
+++ src/series/serie1/Arrays.java
@@ -0,0 +1,23 @@
+package series.serie1;
+
+/**
+ * Created by msousa on 10/4/2016.
+ * M�todos a implementar na s�rie 1
+ */
+public class Arrays{
+
+    public static int greatestAfterRotate(int[] v, int left, int right) {
+        return right;
+    }
+
+    public static boolean isPermutation(int[] a1, int l1, int r1, int[] a2, int l2, int r2) {
+        return false;
+    }
+
+    public static void changeValueInMaxHeap(int[] v, int count, int ix, int newValue){
+    }
+
+    public static void sortIPv4Addresses(String[] v, int l, int r) {
+    }
+
+}
