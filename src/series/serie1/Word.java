27769240753c12667ea90cdaebfee402438b3ace
JpmGameiro <joaogameiro25@gmail.com>
2016-10-25 17:39:52 +0100

    words

---
+++ src/series/serie1/Word.java
@@ -0,0 +1,33 @@
+package series.serie1;
+
+
+public class Word {
+
+    private String wordName;
+    private int count;
+
+    public Word(String wordName, int count){
+        this.wordName = wordName;
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public String getWordName() {
+        return wordName;
+    }
+
+    public void resetCount(){
+        this.count=0;
+    }
+
+    public void setWordName(String wordName) {
+        this.wordName = wordName;
+    }
+
+    public void increment(){
+        this.count++;
+    }
+}
