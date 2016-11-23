462e36707ab8d85eb94fff739850ca6a111f090b
brunofilipe <bruno.filipe1996@hotmail.com>
2016-11-23 19:51:39 +0000

    Parte 1 - feita

--- src/series/serie2/Iterables.java
+++ src/series/serie2/Iterables.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class Iterables {
+
     public static  Iterable<Integer> alternateEvenOdd(final Iterable<Integer> src){
         return () -> new Iterator<Integer>() {
             Iterator<Integer> iterAux = src.iterator();
@@ -46,8 +47,45 @@ public class Iterables {
             }
         };
 	}
+
 	public static <T> Iterable<T> flatten( final Iterable< Iterable<T> > src) {
-		throw new UnsupportedOperationException();
+        return () -> new Iterator<T>() {
+            Iterator<Iterable<T>> iterAux = src.iterator();
+            Iterator<T> it;
+            T curr;
+            {
+                if( iterAux.hasNext() ){
+                   it= iterAux.next().iterator();
+                }
+            }
+
+            @Override
+            public boolean hasNext() {
+                if (it == null)return false;
+                if(curr!= null)return true;
+                while (!it.hasNext() && iterAux.hasNext() ){
+                    it = iterAux.next().iterator();
+                }
+                boolean flag = it.hasNext();
+                if(flag) curr = it.next();
+                return flag;
+            }
+            @Override
+            public T next() {
+                T temp;
+                if (hasNext()){
+                    temp = curr;
+                    curr = null;
+                    return temp;
+                }
+                throw new NoSuchElementException("flatten: no more elements");
+
+            }
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException("flatten: remove not supported");
+            }
+        };
 	}
 
 
--- src/series/serie2/Utils.java
+++ src/series/serie2/Utils.java
@@ -3,7 +3,29 @@ package series.serie2;
 public class Utils {
 	
 	public static <E> Node<E>[] shrink(Node<E>[] hashTable){
-		throw new UnsupportedOperationException();
+        int d = hashTable.length;
+        Node<E>[] newHash = new Node[d/2];
+        int idx = 0;
+        for (int i = 0; i < d; i++) {
+                if (idx == newHash.length) {
+                    idx = 0;
+                }
+                if (newHash[idx] == null) {
+                    newHash[idx] = hashTable[i];
+                } else {
+                    if ( hashTable[i].next != hashTable[i]  ) {
+                        Node<E> oldlast = newHash[idx].previous;
+                        Node<E> newlast = hashTable[i].previous;
+                        Node<E> oldfirst = hashTable[i].next;
+                        oldfirst.previous = oldlast;
+                        oldlast.next = oldfirst;
+                        newHash[idx].previous = newlast;
+                        newlast.next = newHash[idx];
+                    }
+                }
+            ++idx;
+        }
+        return newHash;
 	}
 
 
