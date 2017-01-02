0b7523ac2dadc0833aedf80126c3bdba42b946db
brunofilipe <bruno.filipe1996@hotmail.com>
2017-01-02 19:04:56 +0000

    grafo criado

--- tests/series/serie3/MST.java
+++ tests/series/serie3/MST.java
@@ -1,7 +1,6 @@
 package series.serie3;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -10,14 +9,13 @@ public class MST {
     public Vertex[] readFile(String fileName) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(fileName));
         Vertex[]graph = new Vertex[0];
-
         String line = br.readLine();
         while (line!=null){
             if(line.charAt(0) == 'p'){
                 graph = new Vertex[line.charAt(5) - '0'];
             }
             else if(line.charAt(0) == 'a'){
-               putInVertex(graph,line.charAt(2)-'0',line);
+              putInVertex(graph,line.charAt(2)-'0',line);
             }
             line = br.readLine();
         }
@@ -27,13 +25,13 @@ public class MST {
     public void putInVertex(Vertex[]graph,int idx,String line){
         if(graph[idx] == null){
             graph[idx] = new Vertex(line.charAt(2)-'0');
-            graph[idx].list = new Edge(new Vertex(line.charAt(4)-'0'),line.charAt(6)-'0');
+            graph[idx].list= new Edge(new Vertex(line.charAt(4)-'0'),line.charAt(6)-'0');
         }
         else {
-            while (graph[idx].list.next == null){
-                graph[idx].list.next = graph[idx].list.next.next;
-            }
-            graph[idx].list.next = new Edge(new Vertex(line.charAt(4)-'0'),line.charAt(6)-'0');
+            Edge toInsert = new Edge(new Vertex(line.charAt(4) - '0'), line.charAt(6) - '0');
+            Edge n = graph[idx].list;
+            while (n.next != null) {n = n.next;}
+            n.next = toInsert;
         }
     }
 
